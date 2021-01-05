43a01acf02818fc90dee0105052be20425d3fc99
nosvalds <niko@devinit.org>
2021-01-05 14:19:01 +0000

    add function that uses the --recover --output parameters

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,24 @@ import { spawn } from "child_process";
  *
  * The exit code of xmllint informs us whether the xml was valid or not
  */
-const exec_xmllint = (input: string | Buffer, command: string): Promise<void> =>
+const exec_xmllint = (
+  input: string | Buffer,
+  command: string
+): Promise<void | string> =>
   new Promise((resolve, reject) => {
     const xmllint = spawn(command, { shell: true });
 
     // stdout and stderr are both captured to be made available if the promise rejects
     let output = "";
-    xmllint.stdout.on("data", chunk => (output += chunk.toString()));
-    xmllint.stderr.on("data", chunk => (output += chunk.toString()));
+    xmllint.stdout.on("data", (chunk) => (output += chunk.toString()));
+    xmllint.stderr.on("data", (chunk) => (output += chunk.toString()));
 
     // Any errors cause a rejection
     xmllint.on("error", reject);
 
-    xmllint.on("close", code => {
+    xmllint.on("close", (code) => {
       if (code === 0) {
-        return resolve();
+        return resolve(output);
       }
       return reject(
         new Error(
@@ -41,6 +44,17 @@ const exec_xmllint = (input: string | Buffer, command: string): Promise<void> =>
  */
 export const validateXML = (input: string | Buffer) =>
   exec_xmllint(input, "xmllint --noout --nonet -");
+
+/**
+ * Validate XML without any DTD or schema. Recover XML file.
+ *
+ * @param input XML
+ * @param output File path to store output
+ */
+export const validateXMLrecoverOutput = (
+  input: string | Buffer,
+  output: string | Buffer
+) => exec_xmllint(input, `xmllint --nonet --recover --output ${output} -`);
 
 /**
  * Validate XML with DTD.
