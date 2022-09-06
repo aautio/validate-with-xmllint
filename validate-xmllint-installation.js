f375f2c176207425154493ed21baae44402e6ee1
Ari Autio <ari.autio@iki.fi>
2022-09-06 14:34:04 +0300

    Fix `npm i` failure
    `postinstall`failed while checking out the repo for development.
    The `validate-xmllint-installation` failed to be run, thus relocated it.

--- validate-xmllint-installation.js
+++ validate-xmllint-installation.js
@@ -5,11 +5,14 @@ const { spawnSync } = require("child_process");
  *
  * It verifies that xmllint is present and can be used to perform the
  * xml validation.
+ * 
+ * See FAQ in README.md for xmllint installation instructions.
  */
 if (spawnSync("xmllint --version", { shell: true }).status !== 0) {
-  throw new Error(
-    "Failed to validate xmllint installation. All xml validations will fail."
-  );
+  const errorMsg = "Failed to find xmllint installation. All xml validations will fail. See FAQ in README.md."
+
+  console.error(errorMsg)
+  throw new Error(errorMsg);
 } else {
   console.log("xmllint has been located. Ready to validate xml.");
 }
